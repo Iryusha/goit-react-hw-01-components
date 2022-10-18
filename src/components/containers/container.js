f690afea364900dcaf9f01f92a810096e20fdf6c
Iryusha <Sherbinina_Irina@ukr.net>
2022-10-18 11:29:22 +0300

    fix

--- src/components/containers/container.js
+++ src/components/containers/container.js
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import style from './container.module.css';
 
 export default function Container({ children }) {
   return <div className={style.container}>{children}</div>;
 }
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
